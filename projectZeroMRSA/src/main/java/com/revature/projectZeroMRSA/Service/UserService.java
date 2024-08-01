4117345f3fe0c1775367c76ba62d2b9d833281b8
Sarah Anatoli <ahmad.sarah.333@gmail.com>
2024-08-01 17:52:14 -0400

    user service

---
+++ projectZeroMRSA/src/main/java/com/revature/projectZeroMRSA/Service/UserService.java
@@ -0,0 +1,28 @@
+package com.revature.projectZeroMRSA.Service;
+import com.revature.projectZeroMRSA.Model.Users;
+import com.revature.projectZeroMRSA.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.naming.AuthenticationException;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository){
+        this.userRepository=userRepository;
+    }
+
+    public Users login(String username, String password) throws AuthenticationException {
+        Optional<Users> user=userRepository.findByUsernameAndPassword(username, password);
+        if(user.isPresent()){
+            return user.get();
+        }
+        return null;
+    }
+
+}
